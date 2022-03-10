a69d10472581c369f7fa52f55008d547cc8239f8
cardosoo01 <93734753+cardosoo01@users.noreply.github.com>
2022-03-10 16:33:31 -0300

    teste 20220310

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,14 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'xp',
+    loadChildren: () => import('./pages/xp/xp.module').then( m => m.XpPageModule)
   }
 ];
 
